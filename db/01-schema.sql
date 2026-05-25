320bb0e89e5ea8957df59473c7c9f315f1b6abbc
Marcos Magaña <marcosmh@outlook.com>
2026-05-24 18:32:18 -0600

    modified scripts

--- db/01-schema.sql
+++ db/01-schema.sql
@@ -1,71 +1,81 @@
-CREATE TABLE login_user (
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    password_hash VARCHAR(255),
-    user_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    auth_provider VARCHAR(20) DEFAULT 'local',
-    google_id VARCHAR(255) UNIQUE,
-    role_user VARCHAR(20) DEFAULT 'USER',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_login TIMESTAMP
+CREATE TABLE public.login_user (
+	id int4 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	email varchar(255) NOT NULL,
+	password_hash varchar(255) NULL,
+	user_name varchar(255) NOT NULL,
+	last_name varchar(255) NOT NULL,
+	auth_provider varchar(255) DEFAULT 'local'::character varying NULL,
+	google_id varchar(255) NULL,
+	role_user varchar(255) DEFAULT 'USER'::character varying NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	last_login timestamp NULL,
+	CONSTRAINT login_user_email_key UNIQUE (email),
+	CONSTRAINT login_user_google_id_key UNIQUE (google_id),
+	CONSTRAINT login_user_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.customer (
+	id int4 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	tax_id varchar(255) NOT NULL,
+	name_customer varchar(255) NOT NULL,
+	email varchar(255) NULL,
+	phone varchar(255) NULL,
+	risk_score numeric(5, 2) DEFAULT 0.0 NULL,
+	status varchar(255) DEFAULT 'ACTIVE'::character varying NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	CONSTRAINT customer_pkey PRIMARY KEY (id),
+	CONSTRAINT customer_tax_id_key UNIQUE (tax_id)
 );
 
 
-CREATE TABLE customer (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    tax_id VARCHAR(50) UNIQUE NOT NULL,
-    name_customer VARCHAR(150) NOT NULL,
-    email VARCHAR(150),
-    phone VARCHAR(20),
-    risk_score DECIMAL(5, 2) DEFAULT 0.0,
-    status VARCHAR(20) DEFAULT 'ACTIVE',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE contract (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    customer_id INTEGER NOT NULL,
-    title VARCHAR(100),
-    start_date DATE NOT NULL,
-    end_date DATE,
-    monthly_fee DECIMAL(15, 2) NOT NULL,
-    currency VARCHAR(3) DEFAULT 'USD',
-    status VARCHAR(20) DEFAULT 'ACTIVE',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_customer FOREIGN KEY (customer_id) 
-        REFERENCES customer(id) ON DELETE CASCADE
+CREATE TABLE public.contract (
+	id int4 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	customer_id int4 NOT NULL,
+	title varchar(255) NULL,
+	start_date date NOT NULL,
+	end_date date NULL,
+	monthly_fee numeric(15, 2) NOT NULL,
+	currency varchar(255) DEFAULT 'USD'::character varying NULL,
+	status varchar(255) DEFAULT 'ACTIVE'::character varying NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	CONSTRAINT contract_pkey PRIMARY KEY (id)
 );
 
 
-CREATE TABLE invoice (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    contract_id INTEGER NOT NULL,
-    invoice_number VARCHAR(50) UNIQUE NOT NULL,
-    issue_date TIMESTAMP NOT NULL,
-    due_date TIMESTAMP NOT NULL,
-    total_amount DECIMAL(15, 2) NOT NULL,
-    penalty_amount DECIMAL(15, 2) DEFAULT 0.0,
-    status VARCHAR(20) DEFAULT 'PENDING',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_contract FOREIGN KEY (contract_id) 
-        REFERENCES contract(id) ON DELETE CASCADE
+ALTER TABLE public.contract ADD CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES public.customer(id) ON DELETE CASCADE;
+
+CREATE TABLE public.invoice (
+	id int4 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	contract_id int4 NOT NULL,
+	invoice_number varchar(255) NOT NULL,
+	issue_date timestamp NOT NULL,
+	due_date timestamp NOT NULL,
+	total_amount numeric(15, 2) NOT NULL,
+	penalty_amount numeric(15, 2) DEFAULT 0.0 NULL,
+	status varchar(255) DEFAULT 'PENDING'::character varying NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	CONSTRAINT invoice_invoice_number_key UNIQUE (invoice_number),
+	CONSTRAINT invoice_pkey PRIMARY KEY (id)
 );
 
 
-CREATE TABLE payment (
-     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    invoice_id INTEGER NOT NULL,
-    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    amount_paid DECIMAL(15, 2) NOT NULL,
-    payment_method VARCHAR(50),
-    reference_number VARCHAR(100),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_invoice FOREIGN KEY (invoice_id) 
-        REFERENCES invoices(id) ON DELETE CASCADE
+ALTER TABLE public.invoice ADD CONSTRAINT fk_contract FOREIGN KEY (contract_id) REFERENCES public.contract(id) ON DELETE CASCADE;
+
+CREATE TABLE public.payment (
+	id int4 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	invoice_id int4 NOT NULL,
+	payment_date timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	amount_paid numeric(15, 2) NOT NULL,
+	payment_method varchar(255) NULL,
+	reference_number varchar(255) NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	CONSTRAINT payment_pkey PRIMARY KEY (id)
 );
+
+
+ALTER TABLE public.payment ADD CONSTRAINT fk_invoice FOREIGN KEY (invoice_id) REFERENCES public.invoice(id) ON DELETE CASCADE;
